7d5e9502879438c545559ed74ed5e52b20842be8
Gleb Vishnevsky <vishgleb@mail.ru>
2016-07-25 16:53:27 +0300

    'end' fix

--- lib/gleborator/railtie.rb
+++ lib/gleborator/railtie.rb
@@ -4,9 +4,8 @@ module Gleborator
   class Railtie < Rails::Railtie
 
     initializer "draper.active_record" do |app|
-        ActiveSupport.on_load :active_record do
-          Gleborator.setup self
-        end
+      ActiveSupport.on_load :active_record do
+        Gleborator.setup self
       end
     end
   end
