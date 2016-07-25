a939ab7793770d6220f039897c19d60953afbf8c
Gleb Vishnevsky <vishgleb@mail.ru>
2016-07-25 17:22:38 +0300

    Added collection decorate method

--- lib/gleborator/setup.rb
+++ lib/gleborator/setup.rb
@@ -14,10 +14,10 @@ module Gleborator
     module ClassMethods
       def decorate
         begin
-          decorator_class = "#{self.class.name}Decorator".constantize
+          decorator_class = "#{self.name}Decorator".constantize
           decorate_class.decorate_collection(self)
         rescue NameError => e
-          raise "Couldn't find decorator #{self.class.name}Decorator"
+          raise "Couldn't find decorator #{self.name}Decorator"
         end
       end
     end
