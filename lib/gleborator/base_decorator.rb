9a2e9033b8bbc4d36fcc36bb7758b97f74ab3dd8
Gleb Vishnevsky <vishgleb@mail.ru>
2016-07-26 13:46:43 +0300

    Added delegation

--- lib/gleborator/base_decorator.rb
+++ lib/gleborator/base_decorator.rb
@@ -4,8 +4,13 @@ module Gleborator
       @object = object
     end
 
-    def self.decorate_collection(col)
-      col.map {|object| new(object) }
+    def self.decorate_collection(collection)
+      collection.map {|object| new(object) }
+    end
+
+    def self.delegate(*methods)
+      options = methods.extract_options!
+      super *methods, options.reverse_merge(to: :object)
     end
   end
 end
